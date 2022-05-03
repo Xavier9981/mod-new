6a62258c332e9e87d10d429e0d9aaeadae60d51e
Xavier K Muneku <xavier.muneku@gmail.com>
2022-05-03 11:39:58 +0200

    modified the file

--- src/script/nav-responsive-script.js
+++ src/script/nav-responsive-script.js
@@ -24,15 +24,7 @@ menuCloseBtn.onclick = function () {
 };
 
 // sidebar submenu open close js code
-let htmlcssArrow = document.querySelector(".htmlcss-arrow");
+let htmlcssArrow = document.querySelector(".toggle-sub-menu");
 htmlcssArrow.onclick = function () {
   navLinks.classList.toggle("show1");
 };
-let moreArrow = document.querySelector(".more-arrow");
-moreArrow.onclick = function () {
-  navLinks.classList.toggle("show2");
-};
-let jsArrow = document.querySelector(".js-arrow");
-/*jsArrow.onclick = function () {
-  navLinks.classList.toggle("show3");
-};*/
